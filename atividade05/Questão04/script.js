e6d1ab89550386bb92ab6eabd37a587193b34251
Kleyciane D. <kleyciane.d.mendes@gmail.com>
2025-10-03 18:57:42 -0300

    adicionado questões novas

--- atividade05/Questão04/script.js
+++ atividade05/Questão04/script.js
@@ -1,14 +1,30 @@
+
 document.getElementById('meuFormulario').addEventListener('submit', function (event) {
     event.preventDefault();
 
-    
+// Primeira pergunta
     const respostas01 = document.querySelector('input[name="01pergunta"]:checked');
 
-    if (respostas01) {
-        console.log(`Bebidas: ${respostas01.value}<br>`);
+    if (respostas01.value == "css") {
+        console.log(`Resposta correta: ${respostas01.value}`);
     }
     else {
-        console.log(`Não há bebidas Selecionada<br>`)
+     console.log(`A resposta da pergunta 01 está incorreta`)
     }
 
+
 });
+ 
+// Segunda pergunta
+
+    const respostas02 = document.querySelector('input[name="02pergunta"]:checked');
+
+    if (respostas02 == "java") {
+        console.log(`respostas02: ${respostas02.value}<br>`);
+    }
+    else {
+     console.log(`A resposta da pergunta 02 está incorreta`)
+    }
+
+
+;
